f6203331c38a1fda1f1693bf9ca63f5ee649d7a4
Pavel <epiccinemabattles@gmail.com>
2021-03-15 22:30:55 +0300

    feat: add realization

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const brmap = new Map(bracketsConfig);
+  const open_braskets = [...brmap.keys()];
+  const close_braskets = [...brmap.values()];
+  const stack = [];
+
+  for (let elem of str.split('')) {
+    if (close_braskets.includes(elem) && (elem === brmap.get(stack[stack.length-1])) && stack.length>0){
+      stack.pop();
+    } else if (open_braskets.includes(elem)) {
+      stack.push(elem);
+    } else {
+      return false;
+    } 
+  }
+  return stack.length===0 ? true : false;
 }
